5fb378fc33638f06bbd60ed49878d8f4a0ea91ce
Max McKee <ibathow368@live.com>
2017-11-29 11:46:55 -0800

    Added db brainstorming & listview

--- app/src/main/java/com/example/allwalksoflife/MainActivity.java
+++ app/src/main/java/com/example/allwalksoflife/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.allwalksoflife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.runMenuItem) {
+            Intent runIntent = new Intent(this, CurrentRunActivity.class);
+            startActivity(runIntent);
+            return true;
+        } else if (item.getItemId() == R.id.profileMenuItem) {
+            Intent profileIntent = new Intent(this, UserInfoActivity.class);
+            startActivity(profileIntent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/allwalksoflife/UserInfoActivity.java
+++ app/src/main/java/com/example/allwalksoflife/UserInfoActivity.java
@@ -2,6 +2,7 @@ package com.example.allwalksoflife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ListView runHistoryListView = findViewById(R.id.runHistoryListView);
     }
 }
